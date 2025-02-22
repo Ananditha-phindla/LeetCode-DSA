98d9823a0032929a2d11354427d12cc3f07fbcd9
Ananditha <153907555+Ananditha-phindla@users.noreply.github.com>
2025-02-22 22:40:21 +0530

    Time: 2 ms (69.58%) | Memory: 21.7 MB (55.58%) - LeetSync

---
+++ 1093-recover-a-tree-from-preorder-traversal/recover-a-tree-from-preorder-traversal.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int idx;
+    TreeNode* buildTree(string& traversal,int depth) {
+        if(idx >= traversal.length())
+            return nullptr;
+        int j = idx;
+        while(traversal[j] == '-')
+            j++;
+        int dashes = j-idx;
+        if(depth != dashes)
+            return nullptr;
+        int val = 0;
+        while(j < traversal.length() && traversal[j] != '-') {
+            val = val*10 + (traversal[j]-'0');
+            j++;
+        }
+        TreeNode* node = new TreeNode(val);
+        idx = j;
+        node->left = buildTree(traversal,depth+1);
+        node->right = buildTree(traversal,depth+1);
+        return node;
+    }
+    TreeNode* recoverFromPreorder(string traversal) {
+        int idx = 0;
+        return buildTree(traversal,0);
+    }
+};
