2179530622111efae80bd55c161e807167c60c04
Ananditha <153907555+Ananditha-phindla@users.noreply.github.com>
2025-03-19 23:26:34 +0530

    Time: 5 ms (69.45%) | Memory: 140.2 MB (15.99%) - LeetSync

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
@@ -4,22 +4,22 @@ public:
         int cnt=0;
         int n=nums.size();
         for(int i=0;i<nums.size()-2;i++){
-            if(nums[i]==0){
-                nums[i]=1;
-                if(nums[i+1]==0)
-                    nums[i+1]=1;
+            if(nums[i] == 0){
+                nums[i] = 1;
+                if(nums[i+1] == 0)
+                    nums[i+1] = 1;
                 else
-                     nums[i+1]=0;
-                if(nums[i+2]==0)
-                    nums[i+2]=1;
+                     nums[i+1]= 0;
+                if(nums[i+2] == 0)
+                    nums[i+2] = 1;
                 else
-                     nums[i+2]=0;
+                     nums[i+2] = 0;
                 cnt++;
             }
         }
-        if(nums[n-2]==0 || nums[n-1]==0){
+        if(nums[n-2] == 0 || nums[n-1] == 0){
             return -1;
-            }
+        }
         return cnt;
     }
     
