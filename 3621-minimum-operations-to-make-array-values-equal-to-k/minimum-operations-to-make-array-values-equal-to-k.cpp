273aff181af429f11695004b62c800ad277cfaac
Ananditha <153907555+Ananditha-phindla@users.noreply.github.com>
2025-04-09 21:26:49 +0530

    Time: 13 ms (41.51%) | Memory: 32.4 MB (57.55%) - LeetSync

--- 3621-minimum-operations-to-make-array-values-equal-to-k/minimum-operations-to-make-array-values-equal-to-k.cpp
+++ 3621-minimum-operations-to-make-array-values-equal-to-k/minimum-operations-to-make-array-values-equal-to-k.cpp
@@ -1,23 +1,13 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int k) {
-        vector<bool>cnt(101,false);
-        for(int num : nums){
-            cnt[num] = true;
-        }
-        int i = k-1;
-        while( i >= 0){
-            if(cnt[i])
+        unordered_set<int>myset;
+        for(auto n : nums){
+            if(n < k)
                 return -1;
-            i--;
+            if(n > k)
+                myset.insert(n);
         }
-         i = k+1;
-        int ans = 0;
-        while(i < 101){
-            if(cnt[i])
-                ans++;
-            i++;
-        }
-        return ans;
+        return myset.size();
     }
 };
