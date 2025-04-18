69684f306270dec4ff2ffa3ad98326cac5377f8b
Ananditha <153907555+Ananditha-phindla@users.noreply.github.com>
2025-04-18 20:50:35 +0530

    Time: 6 ms (91.20%) | Memory: 9.5 MB (74.06%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,34 +1,33 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        int len=s.length();
-        string ans = "";
-        if (s.length() == 1){
-            return s ;
-        }
-        int sum = 0;
-        for(int i=0;i<len;i++){
+        int n = s.length();
+        if(n == 1)
+            return s;
+        int len = 0;
+        int l = 0, r = 0;
+        for(int i=0;i<n;i++){
             int p1 = i,p2 = i;
-            while(p1 >= 0 && p2 <= len && s[p1] == s[p2]){
-                if((p2-p1+1) > sum){
-                    ans = s.substr(p1,p2-p1+1);;
-                    sum = p2-p1+1;
-                }
+            while(p1 >= 0 && p2 < n && s[p1] == s[p2]){
+                if(len < (p2-p1+1)){
+                len = (p2 - p1 + 1);
+                l = p1;
+                r = p2-p1+1;
+            }
                 p1--;
                 p2++;
             }
-            p1 = i;
-            p2 = i+1;
-           while(p1 >= 0 && p2 <= len && s[p1] == s[p2]){
-                if(p2-p1+1 > sum){
-                    ans=s.substr(p1,p2-p1+1);;
-                    sum = p2-p1+1;
-                }
+            p1 = i,p2 = i+1;
+            while(p1 >= 0 && p2 < n && s[p1] == s[p2]){
+                if(len < (p2-p1+1)){
+                len = p2 - p1 + 1;
+                l = p1;
+                r = p2-p1+1;
+            }
                 p1--;
                 p2++;
             }
-           // ans=s.substr(p2-p1-1);
         }
-        return ans;
+        return s.substr(l,r);
     }
 };
