f1e66742845708dee29d199195d86ccef6274d24
Ananditha <153907555+Ananditha-phindla@users.noreply.github.com>
2025-07-25 22:51:21 +0530

    Time: 15 ms (70.79%) | Memory: 45.8 MB (93.18%) - LeetSync

---
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    void findNSE(vector<int>& arr,vector<int>& nse){
+        int n = arr.size();
+        stack<int>st;
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && arr[st.top()] > arr[i])
+                st.pop();
+            if(st.empty())
+                nse[i] = n;
+            else
+                nse[i] = st.top();
+            st.push(i);
+        }
+    }
+
+    int sumSubarrayMins(vector<int>& arr) {
+        int mod = 1e9+7;
+        int n = arr.size();
+        vector<int>nse(n);
+        findNSE(arr,nse);
+        long long sum = 0;
+        stack<int>st;
+        
+        for(int i=0;i<n;i++){
+            while(!st.empty() && arr[st.top()] >= arr[i])
+                st.pop();
+            int pse = (st.empty()) ? pse = -1 :  st.top();
+            st.push(i);
+            long long cnt = 0;
+            long long l = (i-pse);
+            long long r = nse[i] - i;
+            cnt = ((l+r-1)+(r-1)*(l-1))*arr[i];
+            sum = (sum + cnt)%mod;
+        }
+        return sum;
+    }
+};
