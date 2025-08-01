0454f92a4cb6a493329fced01e15fb89d6791b8d
Ananditha <153907555+Ananditha-phindla@users.noreply.github.com>
2025-08-01 19:59:55 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (65.93%) - LeetSync

--- 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.cpp
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.cpp
@@ -1,23 +1,17 @@
 class Solution {
 public:
     int timeRequiredToBuy(vector<int>& tickets, int k) {
-        int n = tickets.size();
-        deque<pair<int,int>>dq;
-        for(int i=0;i<n;i++){
-            dq.push_back({tickets[i],i});
-        }
         int time = 0;
-        while(true){
-            if(dq.front().second == k && dq.front().first == 1){
-                time++;
-                break;
+        int n = tickets.size();
+        for(int i=0;i<=k;i++){
+            time += min(tickets[k],tickets[i]);
+        }
+        for(int i=k+1;i<n;i++){
+            if(tickets[i] >= tickets[k]){
+                time += tickets[k]-1;
             }
-            int f = dq.front().first;
-            int i = dq.front().second;
-            dq.pop_front();
-            if(f-1 > 0)
-                dq.push_back({f-1,i});
-            time++;
+            else
+                time += tickets[i];
         }
         return time;
     }
