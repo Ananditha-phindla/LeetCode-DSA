4b03d68fc982ae3b3056851c0f03f22f6acb1ecb
Ananditha <153907555+Ananditha-phindla@users.noreply.github.com>
2025-08-11 14:18:53 +0530

    Time: 94 ms (14.89%) | Memory: 165.2 MB (34.04%) - LeetSync

---
+++ 2529-range-product-queries-of-powers/range-product-queries-of-powers.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    void fillPowers(int n, vector<int>&powers){
+        bitset<32>binary_representation(n);
+        int i=0;
+        while(n > 0){
+            if(n & 1){
+                powers.push_back(1<<i);
+            }
+            i++;
+            n = n >> 1;
+        }
+    }
+    vector<int> productQueries(int n, vector<vector<int>>& queries) {
+        int m = 1e9+7;
+        vector<int>powers;
+        fillPowers(n,powers);
+        vector<int>ans;
+        for(auto q : queries){
+            int l = q[0];
+            int r = q[1];
+            long curr = 1;
+            for(int i=l;i<=r;i++){
+                curr = (curr * powers[i])%m;
+            }
+            ans.push_back(curr);
+        }
+        return ans;
+    }
+};
