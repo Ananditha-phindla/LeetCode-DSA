9629225ab92053e152e18d51f9eb9473833d850d
Ananditha <153907555+Ananditha-phindla@users.noreply.github.com>
2025-08-12 16:39:54 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (70.39%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    double myPow(double x, long n) {
+        if(n < 0){
+            return 1/myPow(x,-n);
+        }
+        double ans = 1;
+        double res = x;
+        while(n){
+            if(n & 1){
+                ans *= res;
+            }
+            res *= res;
+            n = n >> 1;
+        }
+        return ans;
+    }
+};
