5a776bbb6f109fbd233f4011a3d7ffdb439c8338
Ananditha <153907555+Ananditha-phindla@users.noreply.github.com>
2025-08-13 15:14:14 +0530

    Time: 0 ms (100.00%) | Memory: 17 MB (94.91%) - LeetSync

--- 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,22 +1,41 @@
 class Solution {
 public:
-    int kthSmallest(vector<vector<int>>& matrix, int k) {
-        priority_queue<int>pq;
+    int BS(vector<vector<int>>& matrix, int val){
         int n = matrix.size();
         int m = matrix[0].size();
+        int lessElements = 0;
         for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(pq.size() == k){
-                    if(pq.top() > matrix[i][j]){
-                        pq.pop();
-                        pq.push(matrix[i][j]);
-                    }
+            int rowLess = 0;
+            int lo = 0;
+            int hi = m - 1;
+            while(lo <= hi){
+                int mid = (lo + hi)/2;
+                if(matrix[i][mid] < val){
+                    rowLess = mid + 1;
+                    lo = mid + 1;
                 }
-                else if(pq.size() < k){
-                    pq.push(matrix[i][j]);
-                }
+                else
+                    hi = mid - 1;
             }
+            lessElements += rowLess;
         }
-        return pq.top();
+        return lessElements;
+    }
+
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int lo = matrix[0][0];
+        int hi = matrix[n-1][m-1];
+        while(lo <= hi){
+            int mid = (lo + hi)/2;
+            int lessElements = BS(matrix,mid);
+            cout << lessElements << " ";
+            if(lessElements < k)
+                lo = mid + 1;
+            else
+                hi = mid - 1;
+        }
+        return hi;
     }
 };
