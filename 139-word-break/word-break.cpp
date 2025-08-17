f41e8dab674c33e989c057d2992b09a326cbcf39
Ananditha <153907555+Ananditha-phindla@users.noreply.github.com>
2025-08-17 17:15:19 +0530

    Time: 6 ms (61.36%) | Memory: 13.7 MB (59.32%) - LeetSync

---
+++ 139-word-break/word-break.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool solve(string s,unordered_set<string>&myset,int idx,vector<int>&dp){
+        if(idx == s.length()){
+            return true;
+        }
+        if(dp[idx] != -1)
+            return dp[idx];
+        string curr = "";
+        for(int i=idx; i<s.length(); i++){
+            curr += s[i];
+            if(myset.count(curr) && solve(s,myset,i+1,dp)){
+                dp[i] = 1;
+                return true;
+            }
+        }
+        dp[idx] = 0;
+        return false;
+    }
+    bool wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string>myset(wordDict.begin(),wordDict.end());
+        vector<int>dp(s.length(),-1);
+        return solve(s,myset,0,dp);
+    }
+};
