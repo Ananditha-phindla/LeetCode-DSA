e2edef0708ed000d0e37186c881f89b6c9e5c874
Ananditha <153907555+Ananditha-phindla@users.noreply.github.com>
2025-09-04 23:29:20 +0530

    Time: 0 ms (100.00%) | Memory: 97.3 MB (60.44%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,26 +1,15 @@
 class Solution {
 public:
-    int solve(vector<int> &prices,int k,int i,bool buyed,vector<vector<vector<int>>>&dp){
-        if(k == 0 || i >= prices.size())
-            return 0;
-        int inc = 0;
-        int skip =0;
-        if(dp[i][k][buyed] != -1)
-            return dp[i][k][buyed];
-        if(buyed){
-            inc = prices[i] + solve(prices,k-1,i+1,false,dp);
-            skip = solve(prices,k,i+1,buyed,dp);
-        }
-        else{
-            inc = - prices[i] + solve(prices,k,i+1,true,dp);
-            skip = solve(prices,k,i+1,buyed,dp);
-        }
-        return dp[i][k][buyed] = max(inc,skip);
-        
-    }
-
     int maxProfit(vector<int>& prices) {
-         vector<vector<vector<int>>>dp(prices.size(),vector<vector<int>>(2,vector<int>(2,-1)));
-         return solve(prices,1,0,false,dp);
+        int buy = INT_MAX;
+        int n = prices.size();
+        int ans = 0;
+        for(int i=0;i<n;i++){
+            if(buy > prices[i])
+                buy = prices[i];
+            else
+                ans = max(ans,prices[i] - buy);
+        }
+        return ans;
     }
 };
