01ac2eceeaef959d8068bca08be5d513029cd3f2
Ananditha <153907555+Ananditha-phindla@users.noreply.github.com>
2025-09-16 10:03:06 +0530

    Time: 3 ms (54.03%) | Memory: 11.2 MB (51.59%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -1,37 +1,23 @@
 class Solution {
 public:
     string frequencySort(string s) {
-        vector<int> lowCnt(26,0);
-        vector<int> uppCnt(26,0);
-        vector<int>digitCnt(10,0);
-
-        for(auto ch : s){
-            if(ch >= 'a' && ch <= 'z')
-                lowCnt[ch-'a']++;
-            else if(ch >= 'A' && ch <= 'Z')
-                uppCnt[ch-'A']++;
-            else
-                digitCnt[ch-'0']++;
+        unordered_map<char,int> mp;
+        for(char ch : s){
+            mp[ch]++;
         }
 
         priority_queue<pair<int,char>>pq;
-        for(int i=0;i<26;i++){
-            if(lowCnt[i] != 0)
-                pq.push({lowCnt[i],char(i+97)});
-            if(uppCnt[i] != 0)
-                pq.push({uppCnt[i],char(i+65)});
-            if(i < 10 && digitCnt[i] != 0)
-                pq.push({digitCnt[i],char(i+48)});
+        for(auto i : mp){
+            pq.push({i.second,i.first});
         }
 
-        string ans = "";
+         string ans = "";
         while(!pq.empty()){
             int freq = pq.top().first;
             char ch = pq.top().second;
             pq.pop();
             ans.append(freq,ch);
         }
-
         return ans;
     }
 };
