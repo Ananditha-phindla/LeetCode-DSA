d0a3470ab8ff30e98d97dea7d8a50b6c20a355cf
Ananditha <153907555+Ananditha-phindla@users.noreply.github.com>
2025-10-19 17:58:35 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (97.40%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -11,18 +11,19 @@ public:
     }
 
     int tabulation(int n, vector<int>& nums){
-        vector<int> dp(n);
         if(n == 1)
             return nums[0];
-        dp[0] = nums[0];
-        dp[1] = max(nums[0],nums[1]);
+        int prev2 = nums[0];
+        int prev1 = max(nums[0],nums[1]);
 
         for(int i=2;i<n;i++){
-            int include = nums[i] + dp[i-2];
-            int exclude = dp[i-1];
-            dp[i] = max(include,exclude);
+            int include = nums[i] + prev2;
+            int exclude = prev1;
+            int curr = max(include,exclude);
+            prev2 = prev1;
+            prev1 = curr;
         }
-        return dp[n-1];
+        return prev1;
     }
 
     int rob(vector<int>& nums) {
