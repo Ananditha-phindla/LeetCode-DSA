1a7fa9f84136f87b395f8548126e2d14953e518f
Ananditha <153907555+Ananditha-phindla@users.noreply.github.com>
2025-10-19 16:41:15 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (87.06%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -9,11 +9,16 @@ public:
     }
 
     int climbStairs(int n) {
-        vector<int> dp(n+1,-1);
-        dp[0] = dp[1] = 1;
-        for(int i=2;i<=n;i++)
-            dp[i] = dp[i-1] + dp[i-2];
-        return dp[n];
+        //vector<int> dp(n+1,-1);
+        int prev1,prev2;
+        prev1 = prev2 = 1;
+        int curr;
+        for(int i=2;i<=n;i++){
+            curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
         //return memoi(dp,n);
     }
 };
