f7eb77324c9be90121c6b38a85ff058b9a75ae6f
Ananditha <153907555+Ananditha-phindla@users.noreply.github.com>
2025-10-21 14:22:18 +0530

    Time: 3 ms (8.28%) | Memory: 9.4 MB (37.87%) - LeetSync

---
+++ 62-unique-paths/unique-paths.cpp
@@ -0,0 +1,16 @@
+class Solution {
+    int memoi(int i, int j, vector<vector<int>>& dp){
+        if(i < 0 || j < 0)   return 0;
+        if(i == 0 || j == 0)  return 1;
+        if(dp[i][j] != -1)
+            return dp[i][j];
+
+        return dp[i][j] = memoi(i,j-1,dp) + memoi(i-1,j,dp);
+    }
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m, vector<int>(n,-1));
+
+        return memoi(m-1,n-1,dp);
+    }
+};
