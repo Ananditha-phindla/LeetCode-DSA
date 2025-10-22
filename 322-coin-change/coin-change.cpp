ee7713df6100895ea4c4c0b6e1644ff61353ec71
Ananditha <153907555+Ananditha-phindla@users.noreply.github.com>
2025-10-22 17:26:49 +0530

    Time: 59 ms (15.53%) | Memory: 49.8 MB (20.71%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,25 +1,29 @@
 class Solution {
+    int memoi(int idx, int amount,vector<int>& coins,  vector<vector<int>> &dp){
+        if(idx < 0 || amount < 0) return -1;
+        if(amount == 0) return dp[idx][0] = 0;
+        if(dp[idx][amount] != -1)   return dp[idx][amount];
+
+        int take = -1;
+        if(amount >= coins[idx])
+            take = 1 + memoi(idx,amount-coins[idx],coins,dp);
+        int skip = memoi(idx-1,amount,coins,dp);
+        if(take > 0 && skip > 0)
+            return dp[idx][amount] = min(take,skip);
+        else if(skip > 0)
+            return dp[idx][amount] = skip;
+        else if(take > 0) 
+            return dp[idx][amount] = take;
+        return dp[idx][amount] = -2;
+    }
 public:
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        queue<pair<int,int>> q;
-        q.push({amount,0});
-        vector<bool> vis(amount+1,false);
-        vis[amount] = true;
-
-        while(!q.empty()){
-            int a = q.front().first;
-            int dist = q.front().second;
-            q.pop();
-            if(a == 0)
-                return dist;
-            for(int i=0;i<n;i++){
-                if(a - coins[i] >= 0 && !vis[a - coins[i]]){
-                    q.push({a-coins[i],dist+1});
-                    vis[a - coins[i]] = true;
-                }
-            }
-        }
-        return -1;
+        vector<vector<int>> dp(n,vector<int>(amount+1,-1));
+        sort(coins.begin(),coins.end());
+        memoi(n-1,amount,coins,dp);
+        if(dp[n-1][amount] == -2)
+            return -1;
+        return dp[n-1][amount];
     }
 };
