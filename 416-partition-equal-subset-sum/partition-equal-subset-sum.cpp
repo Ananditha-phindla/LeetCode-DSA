78cd11d3f7a18cfd0bfa09709408fa8ce57ac88f
Ananditha <153907555+Ananditha-phindla@users.noreply.github.com>
2025-10-22 12:15:33 +0530

    Time: 179 ms (18.82%) | Memory: 110.4 MB (41.53%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -14,12 +14,12 @@ public:
         if(totalSum % 2)   return false;
         vector<vector<int>> dp(n,vector<int>(totalSum/2+1,0));
 
-        if(nums[0] == totalSum/2)
-            dp[0][totalSum/2] = 1;
-
         for(int i=0;i<n;i++){
             dp[i][0] = 1;
         }
+
+        if(nums[0] <= totalSum/2)
+            dp[0][nums[0]] = 1;
 
         for(int i=1;i<n;i++){
             for(int j=1;j<=totalSum/2;j++){
