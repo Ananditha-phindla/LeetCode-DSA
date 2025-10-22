9a315f557e1bf4c2067e58e11947995f6b5c221d
Ananditha <153907555+Ananditha-phindla@users.noreply.github.com>
2025-10-22 19:02:28 +0530

    Time: 27 ms (43.42%) | Memory: 25.2 MB (31.63%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -13,8 +13,27 @@ public:
     int findTargetSumWays(vector<int>& nums, int target) {
         int n = nums.size();
         int total = accumulate(nums.begin(),nums.end(),0);
-        vector<vector<int>> dp(n,vector<int>(2*total+1,-1));
+        if(target < -total || target > total)
+            return 0;
+        vector<vector<int>> dp(n,vector<int>(2*total+1,0));
+        if(nums[0] == 0)
+            dp[0][total] = 2;
+        else {
+            dp[0][nums[0]+total] = 1;
+            dp[0][-nums[0]+total] = 1;
+        }
 
-        return memoi(n-1,0,target,total,nums,dp);
+        for(int i=1;i<n;i++){
+            for(int j=0;j<=2*total;j++){
+                if(nums[i] <= j)
+                    dp[i][j] += dp[i-1][j-nums[i]];
+                if(j + nums[i] <= 2*total)
+                    dp[i][j] +=  dp[i-1][j+nums[i]];
+            }
+        }
+
+        return dp[n-1][target+total];
+
+        //return memoi(n-1,0,target,total,nums,dp);
     }
 };
