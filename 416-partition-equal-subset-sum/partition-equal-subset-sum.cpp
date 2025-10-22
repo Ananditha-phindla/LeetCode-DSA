79d119ad4627998fa076414293d861f873c5fb63
Ananditha <153907555+Ananditha-phindla@users.noreply.github.com>
2025-10-22 11:55:06 +0530

    Time: 41 ms (94.65%) | Memory: 110.6 MB (33.67%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,18 +1,20 @@
 class Solution {
-public:
-    bool solve(vector<int>& nums,int i,int sum,int currsum,vector<vector<int>>&dp){
-        if(currsum == sum)
-            return true;
-        if(i < 0 || sum < 0)
-            return false;
-        if(dp[i][sum] != -1)
-            return dp[i][sum];
-        return dp[i][sum] = (solve(nums,i-1,sum,currsum,dp) || solve(nums,i-1,sum-nums[i],currsum+nums[i],dp));
+    bool memoi(int i, int curr, int rest, vector<int>& nums, vector<vector<int>>& dp){
+        if(curr == rest)  return true;
+        if(i < 0 || curr > rest)  return false;
+        if(dp[i][curr] != -1)  return dp[i][curr];
+
+        return dp[i][curr] = memoi(i-1,curr+nums[i],rest-nums[i],nums,dp) || memoi(i-1,curr,rest,nums,dp);
     }
+
+public:
     bool canPartition(vector<int>& nums) {
         int n = nums.size();
-        int sum = accumulate(nums.begin(),nums.end(),0);
-        vector<vector<int>>dp(n,vector<int>(sum+1,-1));
-        return solve(nums,n-1,sum,0,dp);
+        int totalSum = accumulate(nums.begin(),nums.end(),0);
+        if(totalSum % 2)   return false;
+        vector<vector<int>> dp(n,vector<int>((totalSum+1)/2,-1));
+
+        return memoi(n-1,0,totalSum,nums,dp);
+
     }
 };
