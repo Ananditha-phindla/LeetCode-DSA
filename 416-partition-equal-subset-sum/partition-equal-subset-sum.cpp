1251ec705e956e0f2cb6393605ece8f75fd23de8
Ananditha <153907555+Ananditha-phindla@users.noreply.github.com>
2025-10-22 12:12:43 +0530

    Time: 174 ms (21.47%) | Memory: 110.5 MB (41.53%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -12,8 +12,25 @@ public:
         int n = nums.size();
         int totalSum = accumulate(nums.begin(),nums.end(),0);
         if(totalSum % 2)   return false;
-        vector<vector<int>> dp(n,vector<int>((totalSum+1)/2,-1));
+        vector<vector<int>> dp(n,vector<int>(totalSum/2+1,0));
 
+        if(nums[0] == totalSum/2)
+            dp[0][totalSum/2] = 1;
+
+        for(int i=0;i<n;i++){
+            dp[i][0] = 1;
+        }
+
+        for(int i=1;i<n;i++){
+            for(int j=1;j<=totalSum/2;j++){
+                if(nums[i] <= j){
+                    dp[i][j] = dp[i-1][j-nums[i]];
+                }
+                dp[i][j] |= dp[i-1][j];
+            }
+        }
+
+        return dp[n-1][totalSum/2];
         return memoi(n-1,0,totalSum,nums,dp);
 
     }
