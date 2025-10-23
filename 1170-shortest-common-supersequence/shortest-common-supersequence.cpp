83e24697c16d4bacb45f7a3a0ae57e8fbf557fa4
Ananditha <153907555+Ananditha-phindla@users.noreply.github.com>
2025-10-23 21:13:32 +0530

    Time: 23 ms (87.72%) | Memory: 26.7 MB (88.93%) - LeetSync

--- 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
@@ -1,61 +1,52 @@
 class Solution {
 public:
-    int memoi(string &s1,string s2,int i,int j, vector<vector<int>>&dp){
-        if(i == s1.length() || j == s2.length())
-            return 0;
-        if(dp[i][j] != -1)
-            return dp[i][j];
-        if(s1[i] == s2[j]){
-            dp[i][j] = 1 + memoi(s1,s2,i+1,j+1,dp);
-        }
-        else{
-            dp[i][j] = max(memoi(s1,s2,i+1,j,dp),memoi(s1,s2,i,j+1,dp));
-        }
-        return dp[i][j];
-    }
     string shortestCommonSupersequence(string str1, string str2) {
-        int n = str1.length();
-        int m = str2.length();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        memoi(str1,str2,0,0,dp);
-        string ans;
-        int i = 0;
-        int j = 0;
-        while(i < n && j < m){
-            if(str1[i] == str2[j]){
-                ans += str1[i];
-                i++;
-                j++;
+        int n1 = str1.length();
+        int n2 = str2.length();
+
+        vector<vector<int>> dp(n1+1,vector<int>(n2+1,0));
+
+        for(int i=1;i<=n1;i++){
+            for(int j=1;j<=n2;j++){
+                if(str1[i-1] == str2[j-1]){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }
+                else
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+
+        string ans = "";
+        int i = n1;
+        int j = n2;
+        while(i > 0 && j > 0){
+            if(str1[i-1] == str2[j-1]){
+                ans += str1[i-1];
+                i--;
+                j--;
             }
             else{
-                if(j+1 < m && i+1 < n){
-                    if(dp[i+1][j] >= dp[i][j+1]){
-                        ans += str1[i];
-                        i++;
-                    }
-                    else{
-                        ans += str2[j];
-                        j++;
-                    }
-                }
-                else if(i+1 < n){
-                    ans += str1[i];
-                    i++;
+                if(dp[i][j-1] >= dp[i][j]){
+                    ans += str2[j-1];
+                    j--;
                 }
                 else{
-                    ans += str2[j];
-                    j++;
+                    ans += str1[i-1];
+                    i--;
                 }
             }
         }
-        while(i < n){
-            ans += str1[i];
-            i++;
+
+        while(i > 0){
+            ans += str1[i-1];
+            i--;
         }
-        while(j < m){
-            ans += str2[j];
-            j++;
+        while(j > 0){
+            ans += str2[j-1];
+            j--;
         }
+
+        reverse(ans.begin(),ans.end());
         return ans;
     }
 };
