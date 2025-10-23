208ce14b35ec0b198441ab9fc3304c909ebb4f5e
Ananditha <153907555+Ananditha-phindla@users.noreply.github.com>
2025-10-23 12:39:17 +0530

    Time: 2396 ms (5.05%) | Memory: 19.6 MB (35.40%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -13,28 +13,42 @@ public:
     int findTargetSumWays(vector<int>& nums, int target) {
         int n = nums.size();
         int total = accumulate(nums.begin(),nums.end(),0);
-        if(target < -total || target > total)
-            return 0;
-        vector<int> prev(2*total+1,0);
-        if(nums[0] == 0)
-            prev[total] = 2;
-        else {
-            prev[nums[0]+total] = 1;
-            prev[-nums[0]+total] = 1;
-        }
+        vector<vector<int>> dp(n,vector<int>(total+1,0));
 
+        if(nums[0] == 0){
+            dp[0][0] = 2;
+        }
+        else{
+            dp[0][0] = 1;
+        }
+        if(nums[0] != 0 && nums[0] <= total)
+            dp[0][nums[0]] = 1;
+        
         for(int i=1;i<n;i++){
-            vector<int> curr(2*total+1,0);
-            for(int j=0;j<=2*total;j++){
-                if(nums[i] <= j)
-                    curr[j] += prev[j-nums[i]];
-                if(j + nums[i] <= 2*total)
-                    curr[j] +=  prev[j+nums[i]];
+            for(int j=0;j<=total;j++){
+                if(nums[i] <= j){
+                    dp[i][j] = dp[i-1][j-nums[i]];
+                }
+                dp[i][j] += dp[i-1][j];
             }
-            prev = curr;
         }
 
-        return prev[target+total];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=total;j++)
+                cout << dp[i][j] << " ";
+            cout << endl;
+        }
+
+        int ways = 0;
+        for(int i=0;i<=total/2;i++){
+            int sum1 = i;
+            int sum2 = total-i;
+            if(abs(sum1-sum2) == abs(target)){
+                ways += dp[n-1][i];
+            }
+        }
+
+        return ways;
 
         //return memoi(n-1,0,target,total,nums,dp);
     }
