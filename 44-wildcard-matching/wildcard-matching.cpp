dc8866c8943c52d8d7e3f3f156e0d36cc1c23447
Ananditha <153907555+Ananditha-phindla@users.noreply.github.com>
2025-10-23 23:11:14 +0530

    Time: 61 ms (10.09%) | Memory: 63.6 MB (5.02%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -5,7 +5,7 @@ class Solution {
         
         return (i < 0);
     }
-    
+
     int solve(int i, int j, string &s1, string &s2, vector<vector<int>>& dp){
         if(i < 0 && j < 0)   return true;
         if(i < 0)   return false;
@@ -25,8 +25,23 @@ public:
     bool isMatch(string s2, string s1) {
         int n1 = s1.length();
         int n2 = s2.length();
-        vector<vector<int>> dp(n1,vector<int>(n2,-1));
+        vector<vector<int>> dp(n1+1,vector<int>(n2+1,0));
+        dp[0][0] = 1;
 
-        return solve(n1-1,n2-1,s1,s2,dp);
+        for(int i=1;i<=n1;i++){
+            if(allStars(i-1,s1))
+                dp[i][0] = 1;
+        }
+
+        for(int i=1;i<=n1;i++){
+            for(int j=1;j<=n2;j++){
+                if(s1[i-1] == s2[j-1] || s1[i-1] == '?')
+                    dp[i][j] = dp[i-1][j-1];
+                else if(s1[i-1] == '*')
+                    dp[i][j] = dp[i][j-1] || dp[i-1][j];
+            }
+        }
+        return dp[n1][n2];
+        //return solve(n1-1,n2-1,s1,s2,dp);
     }
 };
