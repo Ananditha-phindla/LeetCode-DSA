c34067e9f5b8dcbafb62bc4b28f2a026323e1eaa
Ananditha <153907555+Ananditha-phindla@users.noreply.github.com>
2025-10-23 12:51:01 +0530

    Time: 47 ms (35.01%) | Memory: 49.2 MB (33.54%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,28 +1,25 @@
 class Solution {
-    int memoi(int amount,vector<int>& coins, vector<int>& dp){
-        if(amount < 0)  return 1e7;
-        if(amount == 0) return dp[amount] = 0;
-        if(dp[amount] != -1)   return dp[amount];
-
-        int res = INT_MAX;
-        for(auto c : coins){
-            int take = memoi(amount-c,coins,dp);
-            if(take >= 0)
-                res = min(res,take+1);
+    int solve(int idx, int amount, vector<int> &coins,vector<vector<int>> &dp){
+        if(idx == 0){
+            if(amount % coins[idx] == 0)  return amount/coins[idx];
+            return 1e9;
         }
-        
-        if(res >= 1e7)
-            return dp[amount] = -2;
-        return dp[amount] = res;
+        if(dp[idx][amount] != -1)   return dp[idx][amount];
+
+        int take = 1e9;
+        if(amount >= coins[idx])
+            take = 1 + solve(idx,amount-coins[idx],coins,dp);
+        int skip = solve(idx-1,amount,coins,dp);
+        return dp[idx][amount] = min(take,skip);
     }
+
 public:
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<int> dp(amount+1,-1);
-
-        memoi(amount,coins,dp);
-        if(dp[amount] == -2)
+        vector<vector<int>> dp(n,vector<int>(amount+1,-1));
+        int res = solve(n-1,amount,coins,dp);
+        if(res >= 1e9)
             return -1;
-        return dp[amount];
+        return res;
     }
 };
