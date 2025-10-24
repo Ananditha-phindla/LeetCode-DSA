3a9f2137046571e10217a5088b45f6a8dff0b9d8
Ananditha <153907555+Ananditha-phindla@users.noreply.github.com>
2025-10-24 13:56:29 +0530

    Time: 171 ms (54.67%) | Memory: 138.7 MB (55.57%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,40 +1,23 @@
 class Solution {
-    int solve(int i, int buy, int k, vector<int>& prices, vector<vector<vector<int>>>& dp){
-        if(k == 2 || i == prices.size())   return 0;
-        if(dp[i][buy][k] != -1) return dp[i][buy][k];
+    int solve(int i, int t, vector<int>& prices, vector<vector<int>>& dp){
+        if(i == prices.size() || t == 4)   return 0;
+        if(dp[i][t] != -1)  return dp[i][t];
 
-        if(buy == 1){
-            int include = -prices[i] + solve(i+1,0,k,prices,dp);
-            int skip = solve(i+1,1,k,prices,dp);
-            return dp[i][buy][k] = max(include,skip);
+        if(t % 2 == 0){
+            int include = -prices[i] + solve(i+1,t+1,prices,dp);
+            int skip = solve(i+1,t,prices,dp);
+            return dp[i][t] = max(include,skip);
         }
-        int include = prices[i] + solve(i+1,1,k+1,prices,dp);
-        int skip = solve(i+1,0,k,prices,dp);
-        return dp[i][buy][k] = max(include,skip);
+
+        int include = prices[i] + solve(i+1,t+1,prices,dp);
+        int skip = solve(i+1,t,prices,dp);
+        return dp[i][t] = max(include,skip);
     }
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
+        vector<vector<int>> dp(n+1,vector<int>(4,-1));
 
-        for(int i=n-1;i>=0;i--){
-            for(int j=0;j<2;j++){
-                for(int k=1;k>=0;k--){
-                    if(j == 0){
-                        int include = prices[i] + dp[i+1][1][k+1];
-                        int skip = dp[i+1][0][k];
-                        dp[i][j][k] = max(include,skip);
-                    }
-                    else{
-                        int include = -prices[i] + dp[i+1][0][k];
-                        int skip = dp[i+1][1][k];
-                        dp[i][j][k] = max(include,skip);
-                    }
-                }
-            }
-        }
-
-        return dp[0][1][0];
-        //return solve(0,1,0,prices,dp);
+        return solve(0,0,prices,dp);
     }
 };
